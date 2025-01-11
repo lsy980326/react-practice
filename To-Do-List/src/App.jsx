9ce5b2b0b250b00b2e8833fc028f10ad9ad733d3
lsy980326 <cxv963@gmail.com>
2025-01-12 02:55:03 +0900

    init: todolist

---
+++ To-Do-List/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Header from './components/header/Header'
+import Editor from './components/Editor/Editor'
+import List from './components/List/List'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Editor />
+      <List />
+    </div>
+  )
+}
+
+export default App
