7e633c7ee4b979317abc94d5d3664dadc115a7df
lsy980326 <cxv963@gmail.com>
2025-01-12 05:47:54 +0900

    feat: 투두리스트 기능 완성
    - 할 일 추가/삭제/완료 기능 구현
    - mockData를 활용한 초기 데이터 설정
    - 컴포넌트 구조화 (Header, Editor, List)
    
    세부 구현:
    - useState를 활용한 todos 상태 관리
    - useRef를 사용한 id 관리
    - CRUD 기능 구현
      - onCreate: 새로운 할 일 추가
      - onUpdate: 할 일 완료 상태 토글
      - onDelete: 할 일 삭제

--- To-Do-List/src/App.jsx
+++ To-Do-List/src/App.jsx
@@ -2,14 +2,66 @@ import './App.css'
 import Header from './components/header/Header'
 import Editor from './components/Editor/Editor'
 import List from './components/List/List'
+import { useState,useRef} from 'react'
 
+// 초기 데이터 목록
+const mockData = [
+  {
+    id: 0,
+    isDone: false, // 완료 여부
+    content: "React 공부하기",
+    date:new Date().getTime(), // 생성 시간
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "React 공부하기2", 
+    date:new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "React 공부하기3",
+    date:new Date().getTime(),
+  }
+]
 
 function App() {
+  // todos 상태와 업데이트 함수
+  const [todos, setTodos] = useState(mockData) // mockData를 배열로 감싸지 않음
+  const idRef = useRef(3)
+
+  // 새로운 할 일 추가하는 함수
+  const onCraete = (content) => {
+    const newTodo = {
+      id:idRef.current++,
+      isDone:false,
+      content:content, // 입력받은 내용
+      date:new Date().getTime() // 현재 시간
+    }
+
+    // 새로운 할 일을 배열 맨 앞에 추가
+    setTodos([newTodo,...todos])
+  }
+
+
+  const onUpdate =(targetId) => {
+    setTodos(todos.map((todo)=>
+        todo.id === targetId 
+      ? {...todo,isDone:!todo.isDone} 
+      : todo 
+    ))
+  }
+
+  const onDelete = (targetId) => {
+    setTodos(todos.filter((todo)=>todo.id !== targetId))
+  }
+
   return (
     <div className="App">
-      <Header />
-      <Editor />
-      <List />
+      <Header /> {/* 헤더 컴포넌트 */}
+      <Editor onCraete={onCraete}/> {/* 입력 컴포넌트 */}
+      <List todos={todos} onUpdate={onUpdate} onDelete={onDelete}/> {/* 목록 컴포넌트 */}
     </div>
   )
 }
--- To-Do-List/src/components/List/List.jsx
+++ To-Do-List/src/components/List/List.jsx
@@ -1,16 +1,49 @@
 import "./List.css";
 import TodoItem from "../TodoItem/TodoItem";
+import { useState } from "react";
 
-const List =()=>{
+// 할일 목록을 보여주는 컴포넌트임
+const List =({todos,onUpdate,onDelete})=>{
+
+    const [search,setSearch] = useState("")
+
+    const onChangeSearch = (e) => {
+        setSearch(e.target.value)
+    }
+
+    const getFilteredTodos = () => {
+        if(search===""){
+            return todos
+        }
+        return todos.filter((todo)=>{
+            return todo.content.toLowerCase().includes(search.toLowerCase())
+        })
+    }
+    
+    const filteredTodos = getFilteredTodos()
+
     return (
         <div className="List">
             <h4>Todo List 🌱</h4>
-            <input placeholder="검색어를 입력해주세요." />
+            {/* 검색 입력창임 */}
+            <input 
+            value={search}
+            onChange={onChangeSearch}
+            placeholder="검색어를 입력해주세요." />
+
+            {/* 할일 아이템들을 감싸는 영역임 */}
             <div className="TodoWrapper">
-                <TodoItem />
-                <TodoItem />
-                <TodoItem />
+                {/* todos 배열을 순회하며 필터링된 할일 아이템들을 렌더링함 */}
+                {filteredTodos.map((todo)=>{
+                    return (
+                        <TodoItem key={todo.id} 
+                        {...todo}
+                        onUpdate={onUpdate}
+                        onDelete={onDelete}/>
+                    )
+                })}
             </div>
+
         </div>
     )
 }
