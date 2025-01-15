b114f40486d5fd3dda0da436f48803f2975bcb65
lsy980326 <cxv963@gmail.com>
2025-01-16 03:41:25 +0900

    feat: react.memo를 사용하여 성능 최적화
    - Header 컴포넌트에 react.memo를 적용하여 불필요한 리렌더링 방지
    - TodoItem 컴포넌트에 react.memo를 적용하고 비교 함수를 추가하여 props 변경 시에만 리렌더링되도록 최적화

--- To-Do-List/src/components/header/Header.jsx
+++ To-Do-List/src/components/header/Header.jsx
@@ -1,5 +1,9 @@
 import "./Header.css";
+import {memo} from "react"
 
+// memo는 컴포넌트의 props가 변경되지 않으면 리렌더링을 방지하는 고차 컴포넌트입니다
+// Header 컴포넌트는 props를 받지 않으므로 불필요한 리렌더링을 방지하기 위해 memo로 감쌌습니다
+// 부모 컴포넌트가 리렌더링되어도 Header는 다시 렌더링되지 않습니다
 const Header =()=>{
     return (
         <div className="Header">
@@ -9,4 +13,4 @@ const Header =()=>{
     );
 }
 
-export default Header;
+export default memo(Header);
