2baa6c7034420fcdf6ea3b5180226e91fbf54fae
lsy980326 <cxv963@gmail.com>
2025-02-18 11:44:18 +0900

    feat: 감정 이미지 표시 기능 및 레이아웃 CSS 추가
    - 감정 이미지 표시 기능 추가 (util -> get-emotion-image.js)
    - NanumPenScript 폰트 파일 추가
    - 레이아웃 CSS 적용
      - NanumPenScript 폰트 정의
      - html과 body 태그의 기본 스타일 설정
      - 리액트 앱의 루트 요소 스타일 설정
      - 모든 body 내부 요소에 NanumPenScript 폰트 적용

--- DiaryApp/src/App.jsx
+++ DiaryApp/src/App.jsx
@@ -9,6 +9,7 @@ import Home from './pages/Home'
 import New from './pages/New'
 import Diary from './pages/Diary'
 import Notfound from './pages/NotFound'
+import {getEmotionImage} from './util/get-emotion-image'
 
 // 1. "/" : 모든 일기를 조회하는 home 페이지
 // 2. "/new" : 새 일기를 작성하는 페이지
@@ -23,8 +24,18 @@ function App() {
   }
 
 
+  // 이미지를 assets 폴더에 넣어서 사용하면 vite에서 자동으로 최적화해서 처리함(캐싱)
+  // 이미지의 수가 많으면 public 폴더도 이용
   return (
     <>
+    <div>
+      <img src={getEmotionImage(1)} alt="emotion1" />
+      <img src={getEmotionImage(2)} alt="emotion2" />
+      <img src={getEmotionImage(3)} alt="emotion3" />
+      <img src={getEmotionImage(4)} alt="emotion4" />
+      <img src={getEmotionImage(5)} alt="emotion5" />
+    </div>
+
     <div>
       <Link to="/">Home</Link>
       <Link to="/new">New</Link>
@@ -34,7 +45,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/new" element={<New />} />
-      <Route path="/diary" element={<Diary />} />
+      <Route path="/diary/:id" element={<Diary />} />
       <Route path="*" element={<Notfound/>} /> 
     </Routes>
   </>
--- DiaryApp/src/pages/Diary.jsx
+++ DiaryApp/src/pages/Diary.jsx
@@ -1,7 +1,12 @@
+import { useParams } from "react-router-dom";
+
 const Diary = () => {
+    const parmas = useParams();
+    console.log(parmas);
+
     return (
         <div>
-        <h1>Diary</h1>
+            <h1>{parmas.id}Diary</h1>
         </div>
     );
 }
