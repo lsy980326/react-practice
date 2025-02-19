b623204ff59901d320e3318dad51d23c93c8f520
lsy980326 <cxv963@gmail.com>
2025-02-19 16:54:47 +0900

    feat: DiaryList.jsx에 정렬 기능 및 페이지 연결
    - 정렬 타입 상태 관리 및 변경 핸들러 추가, 정렬된 데이터 반환 함수 추가
    - 정렬된 일기 목록 렌더링 및 페이지 연결을 위한 useNavigate 훅 사용

--- DiaryApp/src/components/DiaryList.jsx
+++ DiaryApp/src/components/DiaryList.jsx
@@ -1,19 +1,44 @@
 import Button from './Button';
 import './DiaryList.css';
 import DiaryItem from './DiaryItem';
+import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
 
 const DiaryList = ({data})=>{
+    const nav = useNavigate();
+    const [sortType, setSortType] = useState("latest");
+
+    const onChangeSortType = (e) => {
+        setSortType(e.target.value);
+    }
+
+    const getSortedData = () => {
+        return data.toSorted((a,b)=>{
+            if(sortType === "oldest"){
+                return Number(a.createdDate - b.createdDate);
+            }
+            else{
+                return Number(b.createdDate - a.createdDate);
+            }
+        })
+    }
+
+    const sortedData = getSortedData();
+
     return (
         <div className="DiaryList">
             <div className="menu_bar">
-                <select>
+                <select onChange={onChangeSortType} value={sortType}>
                     <option value={"latest"}>최신순</option>
                     <option value={"oldest"}>오랜된 순</option>
                 </select>
-                <Button text={"새 일기 쓰기"} type={"POSITIVE"}/>
+                <Button 
+                onClick={()=>nav("/new")}
+                text={"새 일기 쓰기"} 
+                type={"POSITIVE"}/>
             </div>
             <div className="menu_wrapper">
-                {data.map((item)=>(
+                {sortedData.map((item)=>(
                     <DiaryItem key={item.id} {...item}/>
                 ))}
             </div>
